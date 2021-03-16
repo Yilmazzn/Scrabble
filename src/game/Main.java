77eb58c631334a5a91560edb88d97f20167d05f0
yuzun <yuzun@mail.uni-mannheim.de>
2021-03-16 14:59:20 +0100

    #8 Set Up to test build
    Signed-off-by: yuzun <yuzun@mail.uni-mannheim.de>
    Signed-off-by: Yilmaz Uzun <yuzun@mail.uni-mannheim.de>

---
+++ src/game/Main.java
@@ -0,0 +1,24 @@
+package game;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+        launch();
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Scrabble 13");
+        Parent root = FXMLLoader.load(this.getClass().getResource("sample.fxml"));
+        stage.setScene(new Scene(root, 350, 200));
+        stage.show();
+    }
+}
