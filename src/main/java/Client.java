115ef5496a4c26d0bf4b8f143cf9e5cfe04ae276
Yilmaz Uzun <yuzun@mail.uni-mannheim.de>
2021-03-19 22:42:17 +0100

    #8 Maven Project Setup
    Signed-off-by: Yilmaz Uzun <yuzun@mail.uni-mannheim.de>

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,24 +1,20 @@
-package game;
-
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
+public class Client extends Application {
     public static void main(String[] args) {
-
         launch(args);
-
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        /*
+
         stage.setTitle("Scrabble 13");
-        //Parent root = FXMLLoader.load(this.getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(this.getClass().getResource("/sample.fxml"));
         stage.setScene(new Scene(root, 350, 200));
         stage.show();
-        */
     }
-
 }
