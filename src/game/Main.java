ecc4885e44a756ebf1be912fa075dd51a7022b0a
Yilmaz Uzun <yuzun@mail.uni-mannheim.de>
2021-03-19 15:33:03 +0100

    #8 Setup Project
    Delete sample.fxml and adjust main --> Starting here
    
    Signed-off-by: Yilmaz Uzun <yuzun@mail.uni-mannheim.de>

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,9 +1,6 @@
 package game;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,9 +13,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        /*
         stage.setTitle("Scrabble 13");
-        Parent root = FXMLLoader.load(this.getClass().getResource("sample.fxml"));
+        //Parent root = FXMLLoader.load(this.getClass().getResource("sample.fxml"));
         stage.setScene(new Scene(root, 350, 200));
         stage.show();
+        */
     }
+
 }
