399ac1b2b2ec7199e2ca2047ce48869ed8907575
Maximilian Luis Wolfgang Netzer <mnetzer@mail.uni-mannheim.de>
2021-04-17 22:39:44 +0200

    #18 GUI Update

--- src/main/java/gui/controllers/JoinGameController.java
+++ src/main/java/gui/controllers/JoinGameController.java
@@ -10,45 +10,47 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/** @author vihofman Controller for joinGame */
 public class JoinGameController {
 
-  public void backToPlayerLobby(MouseEvent mouseEvent) throws IOException {
-    FXMLLoader loader = new FXMLLoader();
-    loader.setLocation(this.getClass().getResource("/views/playerLobbyView"));
-    Parent playerLobbyView = loader.load();
-    PlayerLobbyController controller = loader.getController();
-    controller.InitData();
+    public void gameView(MouseEvent mouseEvent) throws IOException {
+        System.out.println("createGame");
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("/views/gameView.fxml"));
+        Parent gameView = loader.load();
+        GameViewController controller = loader.getController();
 
-    Scene profileControllerScene = new Scene(playerLobbyView);
-    Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
-    window.setScene(profileControllerScene);
-    window.show();
-  }
-  public void exitGame() throws IOException {
-      FXMLLoader loader = new FXMLLoader();
-      loader.setLocation(this.getClass().getResource("/views/exitGame.fxml"));
-      Parent exitGameView = loader.load();
-      // exitGameController controller = loader.getController();
+        Scene welcomeScene = new Scene(gameView);
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(welcomeScene);
+        window.show();
+    }
 
-      Scene exitGameScene = new Scene(exitGameView);
-      Stage window = new Stage();
-      window.initModality(Modality.APPLICATION_MODAL);
-      window.setTitle("Exit Game");
-      window.setScene(exitGameScene);
-      window.setWidth(300);
-      window.setHeight(200);
-      window.showAndWait();
-  }
-  public void startJoiningGame(MouseEvent mouseEvent) throws IOException{
-      FXMLLoader loader = new FXMLLoader();
-      loader.setLocation(this.getClass().getResource("/views/gameView.fxml"));
-      Parent gameView = loader.load();
-      GameViewController controller = loader.getController();
+    public void exitGame() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("/views/exitGame.fxml"));
+        Parent exitGameView = loader.load();
+        // exitGameController controller = loader.getController();
 
-      Scene profileControllerScene = new Scene(gameView);
-      Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
-      window.setScene(profileControllerScene);
-      window.show();
+        Scene exitGameScene = new Scene(exitGameView);
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Exit Game");
+        window.setScene(exitGameScene);
+        window.setWidth(300);
+        window.setHeight(200);
+        window.showAndWait();
+    }
+
+    public void backToPlayScrabble(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("/views/playScrabbleView.fxml"));
+        Parent playScrabbleView = loader.load();
+        PlayScrabbleController controller = loader.getController();
+
+
+        Scene playScrabbleScene = new Scene(playScrabbleView);
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        window.setScene(playScrabbleScene);
+        window.show();
     }
 }
