4fa1388650083cbdfba754fede6a5b94122b4554
Vincent David Hofmann <vihofman@mail.uni-mannheim.de>
2021-05-24 16:14:08 +0200

    #60 settings update

--- src/main/java/gui/controllers/gameSettingsController.java
+++ src/main/java/gui/controllers/gameSettingsController.java
@@ -33,8 +33,8 @@ public class gameSettingsController {
   int jokerID; // represents the amount of jokers in the game
   String dictionaryID; // current dictionary
 
-  String[] values; // array storing values of letters
-  String[] distributions; // array storing distribution of letters
+  private int[] values; // array storing values of letters
+  private int[] distributions; // array storing distribution of letters
 
   public void setModel(Client client, CreateGameController createController) {
     this.client = client;
